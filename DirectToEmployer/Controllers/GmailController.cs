c66621907b724aa637ff314f468380e6db375b00
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2020-01-01 11:46:22 -0600

    Employer can send an email to a jobseeker who has submitted an application for a job posting.

---
+++ DirectToEmployer/Controllers/GmailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DirectToEmployer.Models;
+using System.Web.Mvc;
+
+namespace DirectToEmployer.Controllers
+{
+    public class GmailController : Controller
+    {
+        // GET: Gmail
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Send(Gmail gmail)
+        {
+            gmail.SendEmail();
+            return View();
+        }
+
+    }
+}
---
+++ DirectToEmployer/Migrations/202001011725359_TwelfthMigration.cs
@@ -0,0 +1,16 @@
+namespace DirectToEmployer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TwelfthMigration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
